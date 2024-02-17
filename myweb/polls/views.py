650725febdeab3f12b78c6f9e0a77dc8a6fb3a3a
unoolon <w.goz@bk.ru>
2024-02-17 16:36:27 +0500

    Django commit
    django

---
+++ myweb/polls/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def indexw(request):
+    return HttpResponse("Hello, world.")
